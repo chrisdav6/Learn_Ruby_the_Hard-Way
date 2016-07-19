7966e8ca479313c5f98769155b39888b25590af1
Chris Davis <chris.davis5440@gmail.com>
2016-07-19 14:23:57 -0400

    Third Lesson

---
+++ ex3.rb
@@ -0,0 +1,33 @@
+# This will print the chicken string
+puts "I will now count my chicken dingus!"
+
+# This will add the Hens and Roosters
+puts "Hens #{25 + 30 / 6.to_f}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# This will print the egg string
+puts "Now i will count the eggs"
+
+# This adds all of the eggs
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# This prints a string
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# This compares 2 expressions
+puts 3 + 2 < 5 - 7
+
+# This prints a string and compares 2 numbers
+puts "What is 3 + 2 '#{3 + 2}'"
+puts "What is 5 - 7 '#{5 - 7}'"
+
+# This prints a string
+puts "Oh, that's why it's false"
+
+# This prints a string
+puts "How about some more"
+
+# This compares 2 numbers
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
