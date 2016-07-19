308ba3a515686a0f7874e217630bfcd2fc760741
Chris Davis <chris.davis5440@gmail.com>
2016-07-19 15:23:34 -0400

    Seventh Lesson

---
+++ ex7.rb
@@ -0,0 +1,32 @@
+# Creates a variable of string for value snow
+snow = "snow"
+
+# Prints string mary had a little lamb
+puts "Mary had a little lamb"
+
+# Prints string Its fleece was white as combined with value from snow variable
+puts "It's fleece was white as #{snow}"
+
+# Prints string and everywhere that maery went
+puts "And everywhere that Mary went"
+
+# Prints . ten times
+puts "." *  10
+
+# Creates variables to spell cheeseburger
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Concatenates all variables to Print string CheeseBurger
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
