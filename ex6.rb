70a90272e98cee3ba2ae106f7ca8094a7cd27bb1
Chris Davis <chris.davis5440@gmail.com>
2016-07-19 15:13:40 -0400

    Sixth Lesson

---
+++ ex6.rb
@@ -0,0 +1,44 @@
+# Creates a variable for the amount of types of people
+types_of_people = 10
+
+# Creates a variable that combines the value of types_of_people variable with a string
+x = "There are #{types_of_people} types of people"
+
+# Creates a variable for the string binary
+binary = "binary"
+
+# Creates a variable for the string don't
+do_not = "don't"
+
+# Creates a variable that combines the values of binary and dont variables along with a string
+y = "Those who know #{binary} and those who #{do_not}"
+
+# Prints the variable x
+puts x
+
+# Prints the variable y
+puts y
+
+# Prints the string "I said" combined with variable x
+puts "I said #{x}"
+
+# Prints the string "I also said" combined with the variable y
+puts "I also said #{y}"
+
+# Creates a variable with the value false
+hilarious = false
+
+# Creates a variable that combines a string with the value from hilarious variable
+joke_evaluation = "Isn't that joke funny?! #{hilarious}"
+
+# Prints the joke_evaluation variable
+puts joke_evaluation
+
+# Creates a variable w containing a string
+w = "This is the left side of..."
+
+# Creates a variable e containing a string
+e = "a string with a right side"
+
+# Prints the concationation of variable w + e
+puts w + e
