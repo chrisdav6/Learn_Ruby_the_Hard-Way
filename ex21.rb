beea7d199430ccf90676de37baad2276fa7a2b4a
Chris Davis <chris.davis5440@gmail.com>
2016-07-20 16:16:28 -0400

    Lesson21

--- ex21.rb
+++ ex21.rb
@@ -1,2 +1,43 @@
 # Exercise 21: Functions Can Return Something
 
+# Creates a function that adds a and b arguments then returns the value
+def add(a, b)
+	puts "Adding #{a} + #{b}"
+	return a + b
+end
+
+# Creates a function that subtracts a and b arguments then returns the value
+def subtract(a, b)
+	puts "Subtracting #{a}, - #{b}"
+	return a - b
+end
+
+# Creates a function that multiplies a and b arguments then returns the value
+def multiply(a, b)
+	puts "Multiplying #{a} * #{b}"
+	return a * b
+end
+
+# Creates a function that divides a and b arguments then returns the value
+def divide(a, b)
+	puts "Dividing #{a} / #{b}"
+	return a / b
+end
+
+puts "Let's do some math with functions"
+
+# Creates a variable from that value that is returned from the function when you add arguments
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(120, 2)
+iq = divide(100, 2)
+
+# Prints the values of all the variables that were created from the function return values
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+# A puzzle for extra credit
+puts "Here is a puzzle!"
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+puts "That becomes #{what}. Can you do it by hand?"
