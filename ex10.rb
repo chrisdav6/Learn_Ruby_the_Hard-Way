327ec6b342ff33770aa411fcf59035e1ca256b79
Chris Davis <chris.davis5440@gmail.com>
2016-07-20 08:27:22 -0400

    Tenth Lesson

---
+++ ex10.rb
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."
+perisian_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts perisian_cat
+puts backslash_cat
+puts fat_cat
