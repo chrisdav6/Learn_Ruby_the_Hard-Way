0e0f30b1beb9dd7d939dc4cf42a696b607e858de
Chris Davis <chris.davis5440@gmail.com>
2016-07-21 14:19:37 -0400

    Ruby Testing page

--- test.rb
+++ test.rb
@@ -1,16 +1,20 @@
 number = "42".to_i
 print number
 puts number.instance_of? Fixnum
+Output = 42true
 
 
 number2 = 43.to_s
 print number2
 puts number2.instance_of? String
+Output = 43true
 
 number3 = "44".to_f
 print number3
 puts number3.instance_of? Float
+Output = 44.0true
 
 number4 = 45.to_f
 print number4
 puts number4.instance_of? Float
+Output = 45.0true
