806c40ca7e06851c13834dd24a0b1f12f2b4f0e0
Chris Davis <chris.davis5440@gmail.com>
2016-07-22 10:48:38 -0400

    Lesson26

--- Ex26.rb
+++ Ex26.rb
@@ -1,3 +1,5 @@
+# Exercise 26: Congratulations, Take a Test!
+
 module Ex26
 
 # This function will break up words for us.
@@ -44,9 +46,11 @@ def Ex26.print_first_and_last_sorted(sentence)
 end
 end
 
+# Prints to the screen using escape sequences
 puts "Let's practice everything."
 puts 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
 
+# Creates text block using escape sequences
 poem = <<END
 \tThe lovely world
 with logic so firmly planted
@@ -56,14 +60,17 @@ and requires an explanation
 \n\t\twhere there is none.
 END
 
+# Print poem block to the screen
 puts "--------------"
 puts poem
 puts "--------------"
 
-
+# Calculates expression and saves value into variable
 five = 10 - 2 + 3 - 6
+# Prints value to the screen
 puts "This should be five: #{five}"
 
+# Creates a function with 1 argument and 3 variables, calculates expression and returns value of each variable
 def secret_formula(started)
   jelly_beans = started * 500
   jars = jelly_beans / 1000
@@ -71,13 +78,16 @@ def secret_formula(started)
   return jelly_beans, jars, crates
 end
 
-
+# Creates a variable to use as the function argument
 start_point = 10000
+# Calls the function using new argument variable and stores the value in a new variable
 beans, jars, crates = secret_formula(start_point)
 
+# Prints value of function to the screen
 puts "With a starting point of: #{start_point}"
 puts "We'd have #{beans} beans, #{jars} jars, and #{crates} crates."
 
+# Calls the word manipulation functions and prints them to the screen
 sentence = "All good things come to those who wait."
 words = Ex26.break_words(sentence)
 sorted_words = Ex26.sort_words(words)
