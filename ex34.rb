df2fa3d996f821116c4a57e8db9235a669d3fa11
Chris Davis <chris.davis5440@gmail.com>
2016-08-01 13:34:33 -0400

    Lesson34

--- ex34.rb
+++ ex34.rb
@@ -1,1 +1,8 @@
 # Exercise 34: Accessing Elements of Arrays
+
+animals = ["bear", "ruby", "peacock", "Kangaroo", "whale", "platypus"]
+
+puts "The first element in the animals array is #{animals[0]}"
+puts "The second is %s" % animals[1]
+puts "The third element is #{animals[3]}, and the fourth is #{animals[4]}"
+puts "The last element in the animals array is #{animals[-1]}"
