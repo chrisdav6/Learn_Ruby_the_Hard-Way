95f2b922ba3456afdb9d21ef64aa589b2eef1a30
Chris Davis <chris.davis5440@gmail.com>
2016-08-01 14:55:26 -0400

    Lesson36

--- ex36.rb
+++ ex36.rb
@@ -1,2 +1,24 @@
 # Exercise 36: Designing and Debugging
 
+=begin
+
+Rules for If-Statements
+	
+1. Every if-statement must have an else.
+
+2. If this else should never run because it doesn't make sense, 
+   then you must use a die function in the else that prints out an
+   error message and dies, just like we did in the last exercise. 
+   This will find many errors.
+
+3. Never nest if-statements more than two deep and always try to
+   do them one deep.
+
+4. Treat if-statements like paragraphs, where each if-elsif-else 
+   grouping is like a set of sentences. Put blank lines before and after.
+
+5. Your boolean tests should be simple. If they are complex, 
+   move their calculations to variables earlier in your function and
+   use a good name for the variable.
+	
+=end
