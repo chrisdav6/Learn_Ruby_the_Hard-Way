b5d5e3c6998316505714a40e974901db4bbb9004
Chris Davis <chris.davis5440@gmail.com>
2016-08-02 09:12:09 -0400

    Testing with Arrays

---
+++ testArray.rb
@@ -0,0 +1,13 @@
+# This test will copy a random array value and store it in a variable
+
+# Create Array
+myArray = ["one", "two", "three", "four", "five", "six"]
+
+# Copy random array value from above array and store it in a variable
+randArrayItem = myArray.sample
+
+# Print value of copied random array value
+puts randArrayItem
+
+# Print all data in array
+puts myArray
